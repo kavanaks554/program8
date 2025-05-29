b518873afcb882eb08b0b7b044f71948dbc415eb
Ars288 <rsananya004@gmail.com>
2025-05-29 07:09:55 +0530

    first commit

---
+++ program8/src/main/java/com/kavanaks554/App.java
@@ -0,0 +1,13 @@
+package com.kavanaks554;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
